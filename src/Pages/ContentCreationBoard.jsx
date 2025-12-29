75c296a1965b0bc57a8c33b66654484e0fdaea6f
omoefe <raniem57@gmail.com>
2025-12-29 17:00:25 +0100

    margin in tools page

--- src/Pages/ContentCreationBoard.jsx
+++ src/Pages/ContentCreationBoard.jsx
@@ -114,7 +114,7 @@ const ContentCreationBoard = () => {
         </div>
 
         {/* Content Grid: Responsive columns */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-20">
           {creationOptions.map((option) => (
             <div
               key={option.id}
